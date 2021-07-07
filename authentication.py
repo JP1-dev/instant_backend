54f7fb1102a7a8a5851583d45bf4575c3088fcfd
jp <johannes.p2005@gmail.com>
2021-07-07 21:25:20 +0200

    authentication

--- authentication.py
+++ authentication.py
@@ -1,16 +1,32 @@
-import psycopg2
-from hashlib import sha256
+from hashlib import sha512
+from db_manager import DBManager
 
 
-def connect():
-    conn = psycopg2.connect(
-        host = 'localhost',
-        port = 5432,
-        database = 'instant',
-        user = 'postgres',
-        password = 'pw'
-    )
+class Authenticator():
+    def __init__(self, privkey_path='./priv.key'):
+        with open(privkey_path, 'r') as f:
+            self.privkey = f.read()
+        self.manager = DBManager()
 
-def process_login_request(request):
-    print(request.method)
-    return "ok"
+    def create_token(self, username):
+        return sha512((username + self.privkey).encode()).hexdigest()
+
+    def authenticate_token(self, username, token):
+        return token == sha512((username + self.privkey).encode()).hexdigest()
+
+    def process_login_request(self, request):
+        headers = request.headers
+        username = headers.get('Username')
+        password = headers.get('Password')
+        token = headers.get('Token')
+        if token is not None and username is not None:
+            if self.authenticate_token(username, token):
+                return 'auth success'
+            return 'auth failed'
+        elif username is not None and password is not None:
+            success = self.manager.check_password(username, password)
+            if success:
+                return self.create_token(username)
+            return 'auth failed'
+
+        return "error"
--- main_instant.py
+++ main_instant.py
@@ -3,13 +3,17 @@ import authentication
 
 app = Flask(__name__)
 
+auth = authentication.Authenticator()
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-    rsp = authentication.process_login_request(request)
+    rsp = auth.process_login_request(request)
     return rsp
 
 @app.route('/')
 def index():
     return "up and running:)"
+
+if __name__ == '__main__':
+    app.run(debug=True,host='localhost',port=8080)
 
